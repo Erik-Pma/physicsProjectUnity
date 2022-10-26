8b9d1b121d6537f1b071fad80a3285bfc2baa78a
Erik-Pma <56539093+Erik-Pma@users.noreply.github.com>
2022-10-25 17:12:42 -0700

    d

--- physics car and person contoller/Assets/scripts/MovePlatform.cs
+++ physics car and person contoller/Assets/scripts/MovePlatform.cs
@@ -17,7 +17,7 @@ public class MovePlatform : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (GetComponentInChildren<TitanfallMovement>()) 
         {
